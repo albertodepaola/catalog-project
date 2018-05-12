b7817fadb5c2319a1d4b26feb9edaa838266e70e
Alberto de Paola <albertodepaola@gmail.com>
2018-05-12 20:41:16 -0300

    adding user class to model, still unused

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from flask_appbuilder import Model
 from flask_appbuilder.models.mixins import AuditMixin, FileColumn, ImageColumn
 from sqlalchemy import Column, Integer, String, ForeignKey, Date
 from sqlalchemy.orm import relationship
+from passlib.apps import custom_app_context as pwd_context
 
 """
 
@@ -30,3 +31,19 @@ class Item(Model):
 
     def __repr__(self):
         return f'{self.title} ({self.id}) - {self.category.name}'
+
+
+class User(Model):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32), index=True)
+    password_hash = Column(String(64))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    def __repr__(self):
+        return f'{self.id} - {self.username}'
