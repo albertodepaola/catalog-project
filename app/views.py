822c092a5fa7d7b19995a039e99e857ca4ab8b5b
Alberto de Paola <albertodepaola@gmail.com>
2018-06-02 19:30:27 -0300

    Adding readonly widgets to items views, changing user loggedin verification

--- app/views.py
+++ app/views.py
@@ -1,12 +1,11 @@
 from flask import render_template, session, redirect
 from app import appbuilder, db
 from flask_appbuilder.models.sqla.interface import SQLAInterface
-from flask_appbuilder import ModelView, IndexView
+from flask_appbuilder import ModelView, IndexView, expose, BaseView
 from app.models import Category, Item
 
 from flask_appbuilder.fieldwidgets import BS3TextAreaFieldWidget, TextField
-from app.widgets import BS3TextFieldROWidget
-
+from app.widgets import BS3TextFieldROWidget, BS3TextAreaFieldROWidget, Select2ROWWidget
 
 """
     Create your Views::
@@ -40,26 +39,26 @@ class ItemModelView(ModelView):
     edit_columns = visible_columns
 
     show_fieldsets = [
-                        (
-                            'Summary',
-                            {'fields': ['title', 'category']}
-                        ),
-                     ]
+        (
+            'Summary',
+            {'fields': ['title', 'category']}
+        ),
+    ]
 
-    edit_form_extra_fields = {'description':    TextField('Provide a description',
-                                                description='Item description',
-                                                widget=BS3TextAreaFieldWidget())}
-
-    add_form_extra_fields = {'description': TextField('Provide a description',
+    edit_form_extra_fields = {'description': TextField('Provide a description',
                                                        description='Item description',
                                                        widget=BS3TextAreaFieldWidget())}
+
+    add_form_extra_fields = {'description': TextField('Provide a description',
+                                                      description='Item description',
+                                                      widget=BS3TextAreaFieldWidget())}
 
     def pre_add(self, item):
         user_id = session['user_id']
         item.user_id = user_id
 
     def pre_update(self, item):
-        self.check_logged_user(item)
+        check_logged_user(item)
 
     def prefill_form(self, form, pk):
         # Checks if logged user is the creator
@@ -67,10 +66,12 @@ class ItemModelView(ModelView):
         user_id = session['user_id']
         if category.user_id != int(user_id):
             form.title.widget = BS3TextFieldROWidget()
+            form.description.widget = BS3TextAreaFieldROWidget()
+            # form.category.widget = Select2ROWWidget()
 
     def pre_delete(self, item):
         # checks if user logged is the creator
-        self.check_logged_user(item)
+        check_logged_user(item)
 
 
 class CategoryModelView(ModelView):
@@ -87,7 +88,7 @@ class CategoryModelView(ModelView):
         item.user_id = user_id
 
     def pre_update(self, item):
-        self.check_logged_user(item)
+        check_logged_user(item)
 
     def prefill_form(self, form, pk):
         # Checks if logged user is the creator
@@ -98,8 +99,7 @@ class CategoryModelView(ModelView):
 
     def pre_delete(self, item):
         # checks if user logged is the creator
-        self.check_logged_user(item)
-
+        check_logged_user(item)
 
 
 """
@@ -110,6 +110,7 @@ class CategoryModelView(ModelView):
 @appbuilder.app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html', base_template=appbuilder.base_template, appbuilder=appbuilder), 404
+
 
 db.create_all()
 
@@ -122,4 +123,3 @@ appbuilder.add_view(ItemModelView,
                     "List Items",
                     icon="fa-envelope",
                     category="Catalog")
-
--- app/widgets.py
+++ app/widgets.py
@@ -1,6 +1,19 @@
-from flask_appbuilder.fieldwidgets import BS3TextFieldWidget
+from flask_appbuilder.fieldwidgets import BS3TextFieldWidget, BS3TextAreaFieldWidget, Select2Widget
+
 
 class BS3TextFieldROWidget(BS3TextFieldWidget):
     def __call__(self, field, **kwargs):
         kwargs['readonly'] = 'true'
         return super(BS3TextFieldROWidget, self).__call__(field, **kwargs)
+
+
+class BS3TextAreaFieldROWidget(BS3TextAreaFieldWidget):
+    def __call__(self, field, **kwargs):
+        kwargs['readonly'] = 'true'
+        return super(BS3TextAreaFieldWidget, self).__call__(field, **kwargs)
+
+
+class Select2ROWWidget(Select2Widget):
+    def __call__(self, *args, **kwargs):
+        kwargs['readonly'] = 'true'
+        return super(Select2Widget, self).__call__(*args, **kwargs)
