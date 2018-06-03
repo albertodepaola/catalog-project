52f848135a682279b2a0f6c22d0d88871c1a2bc0
Alberto de Paola <albertodepaola@gmail.com>
2018-06-02 21:51:48 -0300

    removing unused imports

--- app/models.py
+++ app/models.py
@@ -1,8 +1,6 @@
 from flask_appbuilder import Model
-from flask_appbuilder.models.mixins import AuditMixin, FileColumn, ImageColumn
 from sqlalchemy import Column, Integer, String, ForeignKey, Date
 from sqlalchemy.orm import relationship
-from passlib.apps import custom_app_context as pwd_context
 
 """
 
